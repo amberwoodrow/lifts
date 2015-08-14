16581bdbe4927615c189ea0164ac70214c7c723d
amberwoodrow <amberwoodrow@gmail.com>
2015-08-14 15:14:58 -0600

    Cleaned up code a bit

--- js/dom.js
+++ js/dom.js
@@ -2,8 +2,56 @@ $(document).on('ready', function() {
   console.log('sanity check!');
   $('#date').html(dateToDisplay());
 
-  $("input[type=button]").click(function(){
-    $(this).toggleClass("pressed");
+  $('#addExerciseBtn').click(function() {
+    var exerciseSelectedVal = $('#exerciseName option:selected').val();
+    var exerciseSelectedText = $('#exerciseName option:selected').text();
+    var numSets = $('#sets').val();
+    if (onlyOneExcersize(exerciseSelectedVal)) {
+      for (i=0; i<numSets; i++) {
+        count += 1;
+      $('#tableMain').append(
+        '<tr id="' + exerciseSelectedVal + '">' + 
+          '<td class="exercise">' + 
+            exerciseSelectedText + 
+          '</td>' +
+          '<td class="sets">' + 
+            count +
+          '</td>' +
+          '<td class="reps">' + 
+            'reps' +
+          '</td>' +
+          '<td class="weight">' + 
+            'weight' +
+          '</td>' +
+          '<td class="delete">' + 
+            '<button class="btn btn-danger btn-circle remove">' +
+              '<span class="glyphicon glyphicon-remove">' +
+              '</span>' +
+            '</button>' +
+          '</td>' +
+        '</tr>');
+      }
+    }
+    else {
+      flashMessage("<span class='glyphicon glyphicon-remove'></span> Already added " + exerciseSelectedText.toLowerCase(), "error");
+    }
   });
+
+
+  $(document).on('click', '.remove', function() {
+    exerciseNameArr.splice(exerciseNameArr.indexOf($(this).parents('tr').attr('id')), 1);
+    $(this).parents('tr').remove();
+    console.log(exerciseNameArr);
+  });
+
+    function flashMessage(message, idOfWhereToDisplay) {
+      var id = '#' + idOfWhereToDisplay;
+      $(id).html(message);
+      // $(id).slideDown('slow');
+      $(id).css('display','inline-block');
+      $(id).click(function () { 
+        $(id).hide();
+      });
+    }
 });
 
--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,18 @@
-function dateToDisplay () {
+var exerciseNameArr = [];
+
+// Make sure exercise is in array only once before adding to exerciseNameArr.
+function onlyOneExcersize(selected) {
+  if(exerciseNameArr.indexOf(selected) != -1) {
+    return false;
+  }
+  else {
+    exerciseNameArr.push(selected);
+    return true;
+  }
+}
+
+// returns a date with full month name
+function dateToDisplay() {
   var d = new Date();
   var monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
